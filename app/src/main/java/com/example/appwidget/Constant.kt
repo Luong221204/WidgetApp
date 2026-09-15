2a40e633ea871ff421e84bce385bef254e5dcc07
Luong221204 <luongff15@gmail.com>
2026-09-15 15:17:44 +0700

    Update Constant.kt

--- app/src/main/java/com/example/appwidget/Constant.kt
+++ app/src/main/java/com/example/appwidget/Constant.kt
@@ -2,7 +2,7 @@ package com.example.appwidget
 
 object Constant{
     const val IS_FIRST_RUN = "is_first_run_ahahauahau"
-    const val PREF_NAME = "pref_name_ahah"
+    const val PREF_NAME = "pref_name_ahahghjjjjknn"
     const val APP_COUNT = "app_count"
     const val TIME = "time"
     const val TEXT_NEED_TO_CHANGE= "text_need_to_change"
