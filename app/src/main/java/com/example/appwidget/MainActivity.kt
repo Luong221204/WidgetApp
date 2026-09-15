b584c1a52f5d65f310600df2a2cd21a6c820ffd4
Luong <luongff15@gmail.com>
2026-09-15 13:58:31 +0700

    Add time variable in MainActivity

--- app/src/main/java/com/example/appwidget/MainActivity.kt
+++ app/src/main/java/com/example/appwidget/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.core.view.WindowInsetsCompat
 
 
 class MainActivity : AppCompatActivity() {
+    val time = 500
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
